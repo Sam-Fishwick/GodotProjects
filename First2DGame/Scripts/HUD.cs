300d1d8e830bf985a40c0019e8f221e5d7bb8e65
Sam-Fishwick <sam.fishwick@hotmail.com>
2024-05-23 15:52:03 +0100

    removed touchscreen buttons and added VirtualJoystick asset from MarcoFazio

--- First2DGame/Scripts/HUD.cs
+++ First2DGame/Scripts/HUD.cs
@@ -8,6 +8,7 @@ public partial class HUD : CanvasLayer
     public override void _Ready()
     {
         GD.Print("HUD: _Ready called!");
+        GetNode<CanvasLayer>("TouchScreen/UI").Hide();
     }
 
     public override void _Process(double delta)
@@ -54,6 +55,9 @@ public partial class HUD : CanvasLayer
     {
         GD.Print("HUD: Start Button pressed!");
         GetNode<Button>("StartButton").Hide();
+
+        GetNode<CanvasLayer>("TouchScreen/UI").Show();
+
         GD.Print("HUD: StartGame signal emitted!");
         EmitSignal(SignalName.StartGame);
     }
